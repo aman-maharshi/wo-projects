db652c9b5c399b45e511823236251e0d2b43d525
aman-maharshi <amanm4harshi@gmail.com>
2020-10-05 21:55:59 +0530

    blog responsiveness

--- site-1-react/src/components/Blog.js
+++ site-1-react/src/components/Blog.js
@@ -56,6 +56,9 @@ function Blog() {
                     </div>
                 </div>
             </div>
+            <div className="blog__cta">
+                <button className="btn">view all &rarr;</button>
+            </div>
         </section>
     )
 }
