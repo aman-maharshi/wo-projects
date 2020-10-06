bb812a2b9c131e076d42e1f8262cb64947ef7cf5
aman-maharshi <amanm4harshi@gmail.com>
2020-10-06 13:50:04 +0530

    vendors cont. v2

--- site-1-react/src/components/Vendors.js
+++ site-1-react/src/components/Vendors.js
@@ -4,6 +4,62 @@ function Vendors() {
     return (
         <section className="vendors">
             <p className="vendors__title">Vendors</p>
+            <div className="vendors__container">
+                <div className="vendors__item vendor1">
+                    <a href="/">
+                        <div className="vendors__item__content vendors__item__content1"></div>
+                        <div className="vendors__item__title">Vendor</div>
+                    </a>
+                </div>
+                <div className="vendors__item vendor2">
+                    <a href="/">
+                        <div className="vendors__item__content vendors__item__content2"></div>
+                        <div className="vendors__item__title">Vendor</div>
+                    </a>
+                </div>
+                <div className="vendors__item vendor3">
+                    <a href="/">
+                        <div className="vendors__item__content vendors__item__content3"></div>
+                        <div className="vendors__item__title">Vendor</div>
+                    </a>
+                </div>
+                <div className="vendors__item vendor4">
+                    <a href="/">
+                        <div className="vendors__item__content"></div>
+                        <div className="vendors__item__title">Vendor</div>
+                    </a>
+                </div>
+                <div className="vendors__item vendor5">
+                    <a href="/">
+                        <div className="vendors__item__content"></div>
+                        <div className="vendors__item__title">Vendor</div>
+                    </a>
+                </div>
+                <div className="vendors__item vendor6">
+                    <a href="/">
+                        <div className="vendors__item__content"></div>
+                        <div className="vendors__item__title">Vendor</div>
+                    </a>
+                </div>
+                <div className="vendors__item vendor7">
+                    <a href="/">
+                        <div className="vendors__item__content"></div>
+                        <div className="vendors__item__title">Vendor</div>
+                    </a>
+                </div>
+                <div className="vendors__item vendor8">
+                    <a href="/">
+                        <div className="vendors__item__content"></div>
+                        <div className="vendors__item__title">Vendor</div>
+                    </a>
+                </div>
+                <div className="vendors__item vendor9">
+                    <a href="/">
+                        <div className="vendors__item__content"></div>
+                        <div className="vendors__item__title">Vendor</div>
+                    </a>
+                </div>
+            </div>
         </section>
     )
 }
