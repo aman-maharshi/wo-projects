3dbee7eeff7147f7c408d22875c8197028b437a5
aman-maharshi <amanm4harshi@gmail.com>
2020-10-07 16:34:20 +0530

    build test

--- site-1-react/build/service-worker.js
+++ site-1-react/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0b0b6850bc21405ef1d9be984e8c1a8e.js"
+  "./wo-projects/site-1-react/build/precache-manifest.029d53dc2e87f7c47729a52df337842c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./wo-projects/site-1-react/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
